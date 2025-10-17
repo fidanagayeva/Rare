9ffcd0b0422407a44d98b41a6609ebf4d4f3faa5
Fidan Aghayeva <fidan.aghay@gmail.com>
2025-10-17 15:41:33 +0400

    fix: include front folder after removing nested .git

---
+++ front/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello, Rare Project 👋</h1>
+    </main>
+  );
+}
